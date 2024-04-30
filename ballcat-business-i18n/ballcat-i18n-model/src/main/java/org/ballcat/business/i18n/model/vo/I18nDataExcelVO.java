f5900d8050dd9f5cfdca4578deb50a1b70e95b35
Eric <cs.liaow@gmail.com>
2024-04-30 16:27:52 +0800

    :bug:导入新建修改时校验国际化标识不允许为空

--- ballcat-business-i18n/ballcat-i18n-model/src/main/java/org/ballcat/business/i18n/model/vo/I18nDataExcelVO.java
+++ ballcat-business-i18n/ballcat-i18n-model/src/main/java/org/ballcat/business/i18n/model/vo/I18nDataExcelVO.java
@@ -16,6 +16,8 @@
 
 package org.ballcat.business.i18n.model.vo;
 
+import javax.validation.constraints.NotEmpty;
+
 import com.alibaba.excel.annotation.ExcelProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
@@ -34,6 +36,7 @@ public class I18nDataExcelVO {
 	/**
 	 * 语言标签
 	 */
+	@NotEmpty(message = "{i18nMessage.message}：{}")
 	@ExcelProperty(value = "{i18nMessage.languageTag}", index = 0)
 	@Schema(title = "语言标签")
 	private String languageTag;
@@ -41,6 +44,7 @@ public class I18nDataExcelVO {
 	/**
 	 * 国际化标识
 	 */
+	@NotEmpty(message = "{i18nMessage.message}：{}")
 	@ExcelProperty(value = "{i18nMessage.code}", index = 1)
 	@Schema(title = "国际化标识")
 	private String code;
@@ -48,6 +52,7 @@ public class I18nDataExcelVO {
 	/**
 	 * 文本值，可以使用 { } 加角标，作为占位符
 	 */
+	@NotEmpty(message = "{i18nMessage.message}：{}")
 	@ExcelProperty(value = "{i18nMessage.message}", index = 2)
 	@Schema(title = "文本值，可以使用 { } 加角标，作为占位符")
 	private String message;
